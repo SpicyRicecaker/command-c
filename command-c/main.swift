75a9abdfe0f9f9daa9440984a0437310ab49d11f
SpicyRicecaker <awesomenessisgolden@gmail.com>
2022-07-27 20:16:15 -0700

    Initial Commit

--- command-c/main.swift
+++ command-c/main.swift
@@ -1,11 +1,39 @@
 //
 //  main.swift
-//  command-c
+//  ibook-lens
 //
-//  Created by Oliver Li on 7/27/22.
+//  Created by SpicyRicecaker on 7/26/22.
 //
 
 import Foundation
+import Cocoa
 
-print("Hello, World!")
+func commandC () {
+    let commandKeyDownEvent = CGEvent(keyboardEventSource: nil, virtualKey: CGKeyCode(55), keyDown: true)
+    commandKeyDownEvent?.flags = CGEventFlags.maskCommand
+    let cKeyDownEvent = CGEvent(keyboardEventSource: nil, virtualKey: CGKeyCode(8), keyDown: true)
+    cKeyDownEvent?.flags = CGEventFlags.maskCommand
 
+    commandKeyDownEvent?.post(tap: CGEventTapLocation.cghidEventTap)
+    cKeyDownEvent?.post(tap: CGEventTapLocation.cghidEventTap)
+
+    let commandKeyUpEvent = CGEvent(keyboardEventSource: nil, virtualKey: CGKeyCode(55), keyDown: false)
+    commandKeyUpEvent?.flags = CGEventFlags.maskCommand
+    let cKeyUpEvent = CGEvent(keyboardEventSource: nil, virtualKey: CGKeyCode(8), keyDown: false)
+
+    cKeyUpEvent?.flags = CGEventFlags.maskCommand
+
+    commandKeyUpEvent?.post(tap: CGEventTapLocation.cghidEventTap)
+    cKeyUpEvent?.post(tap: CGEventTapLocation.cghidEventTap)
+}
+
+let clipboard = NSPasteboard.general
+let count = clipboard.changeCount
+
+commandC()
+
+ let time: Double = 1/60
+
+while count == clipboard.changeCount {
+    Thread.sleep(forTimeInterval: time)
+}
